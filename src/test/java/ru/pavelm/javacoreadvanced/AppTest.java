71aff1a7771a1f26da6ab866155f9374ad9e59dd
Pavel <pavelmth@gmail.com>
2018-10-29 23:12:46 +0300

    Lesson 03

--- src/test/java/ru/pavelm/javacoreadvanced/AppTest.java
+++ src/test/java/ru/pavelm/javacoreadvanced/AppTest.java
@@ -4,6 +4,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Unit test for simple App.
  */
@@ -16,5 +23,45 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void reit() {
+        ArrayList<String> listFood = new ArrayList<>();
+        listFood.add("cheese");
+        listFood.add("milk");
+        listFood.add("bread");
+        listFood.add("meat");
+        listFood.add("chicken");
+
+        HashSet<String> repeated = new HashSet<>();
+        int n;
+        for (int i = 0; i < listFood.size(); i++) {
+            n = 0;
+            for (String a: listFood) {
+                if (listFood.get(i).equalsIgnoreCase(a)) {
+                    n++;
+                }
+            }
+            if (n > 1) {
+                repeated.add(listFood.get(i));
+            }
+        }
+
+        Iterator<String> iter = listFood.iterator();
+        Iterator<String> iterRepeated = repeated.iterator();
+        while (iter.hasNext() && iterRepeated.hasNext()) {
+            if (iter.next().equalsIgnoreCase(iterRepeated.next())) {
+                iter.remove();
+            }
+        }
+    }
+
+    @Test
+    public void regex() {
+        System.out.println("Введите пароль");
+        String password = "passWord1";
+        Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$");
+        Matcher matcher = pattern.matcher(password);
     }
 }
